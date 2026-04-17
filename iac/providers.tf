2d77231bce397d7e21f675ced9d207fbe741dbe0
angelopohl <angelfrancasanovach@gmail.com>
2026-04-16 20:42:04 -0500

    feat(iac): habilitar entorno dev mediante terraform workspaces y puertos dinamicos

--- iac/providers.tf
+++ iac/providers.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
-provider "docker" {
-  # Configuration options
+
+resource "docker_image" "nginx" {
+  name = "nginx:alpine"
+}
+
+resource "docker_image" "node" {
+  name = "node:18-alpine"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:15-alpine"
 }
--- iac/web.tf
+++ iac/web.tf
@@ -1,19 +1,9 @@
-resource "docker_container" "api" {
-  name  = "api-${terraform.workspace}-01"
-  image = "lab/api"
+resource "docker_container" "web" {
+  name  = "web-${terraform.workspace}"
+  image = "lab/web"
 
-   ports {
-    internal = "80"
+  ports {
+    internal = 80
     external = var.web_port[terraform.workspace]
-  }
-}
-
-resource "docker_container" "api" {
-  name  = "api-${terraform.workspace}-01"
-  image = "lab/api"
-
-   ports {
-    internal = "3000"
-    external = var.api_port[terraform.workspace]
   }
 }
